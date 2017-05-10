f794fa451c751dfb3ff2bd53fbd24bde18aef03b
Mark Hillebrand <mahilleb@microsoft.com>
2017-05-10 22:49:42 +0200

    WIP

--- Tests/EndToEndTests/CNTKv2Python/Examples/language_understanding_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/language_understanding_test.py
@@ -79,8 +79,8 @@ def test_language_understanding(device_id):
     #set_computation_network_trace_level(1)
     set_fixed_random_seed(42) # to become invariant to initialization order, which is a valid change
     # BUGBUG: This ^^ currently seems to have no impact; the two BN models below should be identical in training
-    disable_forward_values_sharing() # TODO remove this, once it works with fw value sharing
     force_deterministic_algorithms()
+    disable_forward_values_sharing() # TODO remove this, once it works with fw value sharing
 
     if device_id >= 0: # BatchNormalization currently does not run on CPU
         # change to intent classifier   --moved up here since this fails, as repro
